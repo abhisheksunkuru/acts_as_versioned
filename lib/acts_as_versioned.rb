6d3e5c8d7806db9bc94e1f16dfff64f4972f5512
abhisheksunkuru <abhishek.s@risingsuntech.net>
2013-03-15 18:00:18 +0530

    correct spell

--- lib/acts_as_versioned.rb
+++ lib/acts_as_versioned.rb
@@ -247,7 +247,7 @@ module ActiveRecord #:nodoc:
 
           before_save :set_new_version
           after_save :save_version
-          after_destory :save_version
+          after_destroy :save_version
           after_save :clear_old_versions
         end
 
